43a401d93741909121317f41346660cf9517d394
xcatliu <xcatliu@gmail.com>
2018-01-16 16:12:38 +0800

    Update docs

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 import crop from '../src/index';
 
 crop(document.getElementById('face'), {
-    width: 400,
-    height: 200
+    width: 200,
+    height: 150
 }, ({ x, y, width, height }) => {
     console.log(x, y, width, height);
     document.body.innerHTML += `<div style="position:absolute; background-color:rgba(255,0,0,0.3); top:${y}px; left:${x}px; width:${width}px; height:${height}px;"></div>`;
